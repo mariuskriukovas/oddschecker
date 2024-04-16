aa596984961e87387454664cde8d86a7eb627415
Marius.Kriukovas <marius.kriukovas@gmail.com>
2024-04-16 12:05:49 +0300

    implementing main.go;

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hashicorp/go-memdb"
+	"oddschecker/controller"
+	"oddschecker/model"
+	"oddschecker/service"
+)
+
+func main() {
+	db, err := memdb.NewMemDB(model.CreateSchema())
+	if err != nil {
+		panic(err)
+	}
+
+	userService := service.NewUserService(db)
+	betService := service.NewBetService(db)
+	oddsService := service.NewOddsService(db, userService, betService)
+	oddsController := controller.NewOddsController(oddsService)
+
+	router := gin.Default()
+
+	oddsRoutes := router.Group("/odds")
+	{
+		oddsRoutes.POST("/", oddsController.PostOdds)
+		oddsRoutes.GET("/:betId", oddsController.GetOddsByBetID)
+	}
+
+	router.Run(":8080")
+}
